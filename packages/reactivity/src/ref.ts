004909f472eeeeedc43a4fbed9227fefd3e0ee24
wangkb <wangkb@szphps.com>
2024-02-20 14:26:57 +0800

    ✨ feat: complete ref

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,5 +1,7 @@
 import { isObject } from '../../shared/src/index'
 import { reactive } from './reactive'
+import { activeEffect, triggerEffects } from './effect'
+import { Dep, createDep } from './dep'
 
 export function ref(value: any) {
   return createRef(value, false)
@@ -9,10 +11,10 @@ function isRef(value: any) {
   return !!(value && value.__v_isRef)
 }
 
-function toRaw(observed: any): any {
-  const raw = observed && observed['__v_raw']
-  return raw ? toRaw(raw) : observed
-}
+// function toRaw(observed: any): any {
+//   const raw = observed && observed['__v_raw']
+//   return raw ? toRaw(raw) : observed
+// }
 
 function toReactive(value: any) {
   return isObject(value) ? reactive(value) : value
@@ -25,25 +27,42 @@ function createRef(value: any, isShallow: boolean) {
   return new RefImpl(value, isShallow)
 }
 
-function trackRefValue(ref: RefImpl) {}
+function trackRefValue(ref: RefImpl) {
+  if (!ref.dep) {
+    ref.dep = createDep()
+  }
+  if (activeEffect) {
+    ref.dep.add(activeEffect)
+  }
+}
+
+function triggerRefValue(ref: RefImpl) {
+  if (ref.dep) {
+    triggerEffects(ref.dep)
+  }
+}
 
 class RefImpl {
   public __v_isShallow: boolean
-  public dep: undefined
+  public dep: undefined | Dep
   public __v_isRef: boolean
-  public _rawValue: any
   public _value: any
 
   constructor(value: any, isShallow: boolean) {
     this.__v_isShallow = isShallow
     this.dep = undefined
     this.__v_isRef = isRef(value)
-    this._rawValue = isShallow ? value : toRaw(value)
     this._value = isShallow ? value : toReactive(value)
   }
 
   get value() {
     console.log(this._value)
+    trackRefValue(this)
     return this._value
+  }
+
+  set value(newValue) {
+    this._value = this.__v_isShallow ? newValue : toReactive(newValue)
+    triggerRefValue(this)
   }
 }
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,6 @@
 export const isArray = (a: unknown) => Array.isArray(a)
 
 export const isObject = (val: any) => val !== null && typeof val === 'object'
+
+export const hasChanged = (value: any, oldValue: any): boolean =>
+  !Object.is(value, oldValue)
